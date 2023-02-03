c791586d032e96ac8aa7f47c3183f981dc93819b
trp86 <trp86>
2023-02-03 14:19:36 +0000

    Initial Commit

---
+++ databricks-practise/CodeSamples.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+dbutils.fs.help()
+
+# COMMAND ----------
+
+dbutils.fs.mount(
+source = 'wasbs://raw@blobstrgtrp86.blob.core.windows.net/',
+mount_point = '/mnt/raw/',
+extra_configs = {'fs.azure.account.key.blobstrgtrp86.blob.core.windows.net':'O5RCSg41Bil39BpcNFTyg17AIK6C9YbbSRgOpOCS4TBs+DEeDCh5eUUED+NiCxa0wZj8CrdhSkfN+AStJYiXJA=='} 
+)
+
+# COMMAND ----------
+
+dbutils.fs.ls ('/mnt/raw/')
